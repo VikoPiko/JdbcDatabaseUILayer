77a8252d7c57387cd69cdd0a4d26572d5d17ba16
VikoPiko <vikoasmatiko@gmail.com>
2026-01-30 17:31:08 +0200

    Styling UI

--- src/com/ru/mag/db/jdbc/controllers/ListingController.java
+++ src/com/ru/mag/db/jdbc/controllers/ListingController.java
@@ -3,22 +3,34 @@ package com.ru.mag.db.jdbc.controllers;
 import com.ru.mag.db.jdbc.queries.ListingQueries;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class ListingController {
+public class ListingController implements Initializable {
     @FXML
     private TextField type;
     @FXML private TextField description;
     @FXML private TextField notes;
+
+    @FXML
+    private ComboBox<String> statusBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        statusBox.getItems().addAll("ACTIVE", "SOLD", "RENTED");
+    }
 
     ListingQueries repo = new ListingQueries();
 
